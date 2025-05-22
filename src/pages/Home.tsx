4554aa0aef97004ba7f0ada832c358e01369f3e5
TheCodingTom <dastolto.t@gmail.com>
2025-05-22 15:16:38 +0200

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,18 @@
+// import guy from "../images/guy.png";
+import test from "../images/test.png";
+import blacklogo from "../images/blacklogo.png";
+
+function Home() {
+  return (
+    <>
+      <div className="logo-container">
+        <img className="main-logo" src={blacklogo} alt="black logo" />
+      </div>
+      <div className="image-container">
+        <img src={test} className="main-image" alt="image of a tattoed guy" />
+      </div>
+    </>
+  );
+}
+
+export default Home;
