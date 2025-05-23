309b01439cffd79ca5694a3c616868e68f702dd0
TheCodingTom <dastolto.t@gmail.com>
2025-05-23 08:44:53 +0200

    installed shadcn and added dropdown menu

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,17 +1,16 @@
-import React from "react";
-
-import test from "../images/test.png";
 import guy from "../images/guy.png";
 import blacklogo from "../images/blacklogo.png";
+import Dropdown from "@/components/Dropdown";
 
 function Test() {
   return (
     <div id="wrapper">
       <div className="logo-container">
         <img className="main-logo" src={blacklogo} alt="black logo" />
+        <Dropdown />
       </div>
       <div id="a" className="first-panel">
-        <img src={test} className="main-image" alt="image of a tattoed guy" />
+        <img src={guy} className="main-image" alt="image of a tattoed guy" />
       </div>
 
       <div id="b" className="panels">
